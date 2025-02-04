a2ffb94a4e9453767ed7735af73a1851eb494a71
Bianca Goodwin <115598459+Startnow08@users.noreply.github.com>
2025-02-03 19:30:56 -0500

    lesson 12 edits for functionality

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ body.appendChild(footer)
 //Add a copyright to the footer
 const today = new Date();
 const year = today.getFullYear();
-const copyright = document.createElement('')
+const copyright = document.createElement('p')
 copyright.innerHTML = `<span>&#169 ${year} Bianca<\span>`
 footer.appendChild(copyright)
 
@@ -21,3 +21,42 @@ for (let skill of skillsList){
     skillsLI.innerHTML = skill;
     skillsUL.appendChild(skillsLI);
 }
+
+const messageForm = document.querySelector("[name='leave_message']");
+
+
+messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let name = event.target.userName.value;
+    let email = event.target.UsersEmail.value;
+    let message = event.target.usersMessage.value;
+
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Message:", message);
+    let messageSection = document.getElementById("messages");
+    const messageList = messageSection.querySelector("ul");
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href="malito: ${email}">${name} </a><span>wrote: ${message} </span>`
+    messageForm.reset();
+
+    const removeButton = document.createElement("BUTTON");
+    removeButton.innerText = "Remove";
+})
+
+
+
+
+
+function onRemoveButton(event){
+    const entry = event.target.parentNode;
+    entry.remove();
+    removeButton.setAttribute("type", "button");
+    removeButton.addEventListener("click", onRemoveButton);
+
+    
+}
+
+
+newMessage.appendChild(removeButton);
+messageList.appendChild(newMessage);
